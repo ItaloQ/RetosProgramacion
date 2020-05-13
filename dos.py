9fa1f634c02b9bf8b1242ab6b39d9efafbfac727
Italo Quevedo <italo2710@gmail.com>
2020-05-12 21:09:23 -0500

    dos

---
+++ dos.py
@@ -0,0 +1,7 @@
+def Saludo():
+    nombre = input("Escriba su nombre? : ")
+    apellido = input("Escriba su primer apellido? : ")
+    print(f'Hola, {nombre} {apellido}')
+
+if __name__ == "__main__":
+    Saludo()
